19290bc05753f8aed69cd708dab89b57ebbf07f2
Stacy Nowak <nowak.stacy@gmail.com>
2019-07-09 12:49:28 -0700

    initial file setup

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+ 
+app.use(routes);
+ 
+app.listen(PORT);
